5d6cb862ec4103d04baad13e2609b44fea8a3816
Rustem Ishmakov <dartrust@gmail.com>
2022-12-08 13:36:31 +0300

    corrected file

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -8,10 +8,24 @@ const expressionAndAnswer = () => {
   let rightAnswer = '';
   const arr = randomizer();
   const number = arr[0];
-  if (number % 2 === 0 || number % 3 === 0 || number % 5 === 0 || number % 7 === 0) {
-    rightAnswer = 'no';
-  } else rightAnswer = 'yes';
-  return [number, rightAnswer];
+  if (number === 1) {
+    rightAnswer = 'yes';
+  }
+  if ((number % 2 === 0
+    || number % 3 === 0
+    || number % 5 === 0
+    || number % 7 === 0)
+    && (number !== 2
+      || number !== 3
+      || number !== 5
+      || number !== 7)
+    ) {
+      rightAnswer = 'no';
+    }
+   else
+  rightAnswer = 'yes';
+
+return [number, rightAnswer];
 };
 
 const brainPrime = () => {
