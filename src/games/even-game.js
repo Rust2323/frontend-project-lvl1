8fa854727999c0857d64affe91563b4e54afc772
Rustem Ishmakov <dartrust@gmail.com>
2023-01-30 21:12:38 +0600

    make boolean function

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -2,7 +2,7 @@ import gameBody from '../index.js';
 
 import getRandomNumber from '../utils.js';
 
-const isEven = (number) => (number % 2 === 0 ? true : false);
+const isEven = (number) => (number % 2 === 0);
 
 const task = 'Answer "yes" if the number is even, otherwise answer "no".';
 
@@ -10,7 +10,7 @@ const getQuestionAndAnswer = () => {
   const question = getRandomNumber();
   let answer = 'no';
   const booleanAnswer = isEven(question);
-  if(booleanAnswer === true){
+  if (booleanAnswer === true) {
     answer = 'yes';
   }
 
