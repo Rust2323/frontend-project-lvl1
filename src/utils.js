f30843b8ac4d877fea4f69512876555012a86d21
Rustem Ishmakov <dartrust@gmail.com>
2023-01-31 17:27:37 +0600

    change export type

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
-const getRandomNumber = (start = 1, end = 100) => {
+/* eslint-disable import/prefer-default-export */
+
+export const getRandomNumber = (start = 1, end = 100) => {
   const rightEnd = end - start;
   return Math.floor(start + (Math.random() * (rightEnd)));
 };
-
-export default getRandomNumber;
