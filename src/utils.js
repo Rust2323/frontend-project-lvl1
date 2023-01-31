a9110ece6f75cd5b1a1801ca270d993f6959d8e9
Rustem Ishmakov <dartrust@gmail.com>
2023-01-31 17:44:36 +0600

    created new function transforming boolean to string answer

--- src/utils.js
+++ src/utils.js
@@ -4,3 +4,5 @@ export const getRandomNumber = (start = 1, end = 100) => {
   const rightEnd = end - start;
   return Math.floor(start + (Math.random() * (rightEnd)));
 };
+
+export const getAnswer = (booleanAnswer) => (booleanAnswer ? 'yes' : 'no');
